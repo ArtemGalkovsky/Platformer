282130cd1a6eae5f8f5026c18688dc22683df597
ArtemGalkovsky <art_galkovsky@mail.ru>
2020-10-17 17:28:10 +0300

    Updtae

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import eel
 
+
 @eel.expose
 def get_map_file(file_dir):
     with open(file_dir, "r") as fl:
         return fl.read().splitlines()
 
-eel.init("web")
+
+eel.init("web", js_result_timeout=1)
 eel.start("index.html")
-
-
-
--- web/JS/app.js
+++ web/JS/app.js
@@ -1,72 +1,64 @@
 import {canv, ctx, draw} from "./canvas.js"
 import Platform from "./classes.js"
 
-//const map = 
-//function getMap () {
-
 var MAP = []
+var Levels
 
-function getMapFile (map) {
-	MAP = map
-	//console.log(MAP)
-	mapDecoder(MAP, {}, canv.width, canv.height)
+function getMapFile () {
+	console.log('get')
+	eel.get_map_file("web/JS/maps.map")().then(file =>{
+		mapDecoder(file, {}, canv.width, canv.height)
+	})
+	//mapDecoder(mapDecoder(file.then(file => {}), {}, canv.width, canv.height))
+	//console.log(file.then(value => {get})
+	//console.log('End')
+	//file = file.then(file => {
+		//MAP = getMapFile(Array.from(file))
+	//	console.log('FILE GET', file)
+		//return file
+	//})
+	//console.log(file, "f")
+	//console.log(file["[[PromiseResult]]"])
 }
 
-eel.get_map_file("web/JS/maps.map")().then(file => {
-	getMapFile(Array.from(file))
-})
-
-//console.log(MAP, "M")
-
-//console.log(getMapFile(), "l")
-//var mapFile = eel.get_map_file("web/JS/maps.map")()
-//console.log(mapFile.then(file => {return file}))
-//let mapFile = eel.get_map_file("web/JS/maps.map")()
-//mapFile.then(file => {
-//	return file
-	//console.log(file, mapFile)
+//let file = eel.get_map_file("web/JS/maps.map")()
+//file.then(file => {
+//	MAP = getMapFile(Array.from(file))
+//	console.log('FILE GET', MAP)
 //})
-//}
+getMapFile()
 
-//console.log(mapFile.then(file => {console.log(file)}))
+//console.log(MAP, "MAP")
 
-//getMap()
+// mapDecoder(MAP, {}, canv.width, canv.height)
 
 function mapDecoder (mapFile, levelObject, levelWidth, levelHight) {
-	//console.log(mapFile)
 	var row = 0
 	var level
-	for (let line of mapFile) {//= 0; line < mapFile.length; line++) {
-		//console.log(line, "line")//(mapFile[line], "l")
+	for (let line of mapFile) {
 		if (line.includes("#LEVEL")) {
 			level = line.slice(line.indexOf("$") + 1, line.indexOf("$", line.indexOf("$") + 1))
 			levelObject[level] = {}
 			row = 0
-			//console.log('LEVEL LINE', line, levelObject)
 		}  else {
-		levelObject[level][row] = line.trim().split("")//.forEach((item, index) => {
+		levelObject[level][row] = line.trim().split("")
 		levelObject[level][row].forEach((item, index) => {
 			console.log(levelObject[level][row][item])
 			if (item === "-") {
 				levelObject[level][row][index] = new Platform(100, 100, 100, 100, ctx)
 			}
 		})
-		//	if 
-		//})
-		// for (let item in levelObject[level][row].split()) {
-		// 	if (item == 0) {
-		// 		levelObject
-		// 	} 
-		// }
 		row++
-		//console.log(row)
 		}
 	}
 	console.log(levelObject)
+	Levels = levelObject
+	render()
 }
 
 function render (LEVELS, ) {
-	
+	console.log(LEVELS, "L")
+	//requestAnimationFrame(render)
 }
 
-//mapDecoder(mapFile, {}, canv.width, canv.height)
+//render(Levels, "L")//levelObject)
